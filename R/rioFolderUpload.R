782b076dca2a77a332acdee1193d76dee33eeaea
himanshu-sikaria <himanshu.sikaria@socialcops.com>
2018-12-05 14:16:17 +0530

    added folder upload

---
+++ R/rioFolderUpload.R
@@ -0,0 +1,72 @@
+#' Upload a file from the local system to cloud
+#' @description Write a local file to the cloud, S3 or GCS
+#' @param localfolder path of the folder in which all the files are to be uploaded
+#' @param bucketpath path of the folder in which the files are to be uploaded
+#' @param data_source the name of the data source, if not set globally. can be gcs or s3
+#' @param bucket the name of the bucket, if not set globally
+#' @param ... other parameters for gcs/s3 upload
+#'
+#' @export "rioFolderUpload"
+#' @return the filename and path of the file in the bucket
+#' @import "googleCloudStorageR" "aws.s3" "assertthat"
+#' @examples
+#' \dontrun{
+#' rioSetDataSource("gcs")
+#' rioSetBucket("socialcops-test")
+#' rioFileUpload("~/Downloads/", "tests/")
+#' }
+
+rioFolderUpload <- function(localfolder, bucketpath, pattern = "*", overwrite = TRUE, data_source = rioGetDataSource(),
+                          bucket = rioGetBucket(data_source), ...){
+  # Starting data checks --
+  ## valid inputs
+  assert_that(is.character(localfolder),
+              is.character(bucketpath))
+
+  ## data source should be either GCS or S3
+  assert_that(str_to_lower(data_source)%in%c("gcs","s3"),
+              msg = "Data source should be either GCS or S3")
+  data_source = str_to_lower(data_source)
+
+  ## its not a folder and only a file to upload
+  assert_that(tools::file_ext(localfolder) == "" & tools::file_ext(bucketpath) == "",
+              msg = "Cannot upload a file. Make sure its a folder")
+
+  ## file to upload exists
+  file_upload = list.files(localfolder, pattern = pattern, full.names = T, recursive = T)
+  assert_that(length(file_upload)>0, msg = "Please enter a valid local folder with files")
+
+  #removing extra /
+  bucketpath = gsub("\\/+","/",bucketpath)
+
+  # if overwrite or not
+  if(!isTRUE(overwrite)){
+    #removing extra /
+    file_upload = gsub("\\/+","/",file_upload)
+    localfolder = gsub("\\/+","/",localfolder)
+
+    bucketfiles = rioListFiles(path = bucketpath, pattern = pattern, recursive = T,
+                              data_source = data_source, bucket = bucket)
+    bucketfiles = gsub(paste0("^",bucketpath), "", bucketfiles, fixed = T)
+    bucketfiles = gsub("^\\/+","",bucketfiles)
+    localfiles = gsub(paste0("^",localfolder), "", file_upload, fixed = T)
+    localfiles = gsub("^\\/+","",localfiles)
+    commonfiles = which(localfiles %in% bucketfiles)
+    if(length(commonfiles)>0){
+      file_upload = file_upload[-commonfiles,]
+      assert_that(length(file_upload)>0, msg = "All files already exists")
+    }
+  }
+
+  # upload the file if everything is fine
+  for(i in 1:length(file_upload)){
+    bucketpath = gsub("\\/+$","",bucketpath)
+    if(data_source == "gcs"){
+      upload_return = gcs_upload(file = file_upload[i], name = paste0(bucketpath,"/", basename(file_upload[i])), bucket = bucket, ...)
+    } else if(data_source == "s3"){
+      upload_return = aws.s3::put_object(file = file_upload[i], bucket = bucket, object =  paste0(bucketpath,"/", basename(file_upload[i])), ...)
+    }
+  }
+  return(invisible(bucketpath))
+}
+
