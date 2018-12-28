d572c90dad50ed3ddddfd1f536b8211260e262ae
Himanshu <himanshu@Administrators-MacBook-Pro.local>
2018-12-28 23:44:08 +0530

    fix #17

--- R/import_rda.R
+++ R/import_rda.R
@@ -1,27 +1,27 @@
-#' Read RDS file
-#' @description Read R data - RDS file from anywhere
+#' Read RDA file
+#' @description Read RData or rda file from anywhere
 #' @param file path of the file to be read
 #' @param FUN the function using which the file is to be read
 #' @param data_source the name of the data source, if not set globally. s3, gcs or local
 #' @param bucket the name of the bucket, if not set globally
 #' @param ... other parameters for the FUN function defined above
-#' @export "import_rds"
+#' @export "import_rda"
 #' @return the output of the FUN function
 #'
 #' @examples
 #' \dontrun{
 #' flyio_set_datasource("gcs")
 #' flyio_set_bucket("socialcops-test")
-#' import_rds("tests/googletest.rds", readRDS)
+#' load("tests/googletest.rda")
 #' }
 
-import_rds <- function(file, FUN = readRDS, data_source = flyio_get_datasource(),
-                       bucket = flyio_get_bucket(data_source), ...){
+import_rda <- function(file, FUN = load, data_source = flyio_get_datasource(),
+                       bucket = flyio_get_bucket(data_source), envir = globalenv(), ...){
 
   # checking if the file is valid
-  assert_that(tools::file_ext(file) %in% c("RDS", "rds"), msg = "Please input a valid path")
+  assert_that(tools::file_ext(file) %in% c("rda", "Rda", "RData"), msg = "Please input a valid path")
   if(data_source == "local"){
-    t = FUN(file, ...)
+    FUN(file, envir = envir, ...)
     return(t)
   }
   # a tempfile with the required extension
@@ -32,7 +32,7 @@ import_rds <- function(file, FUN = readRDS, data_source = flyio_get_datasource()
   downlogical = import_file(bucketpath = file, localfile = temp, bucket = bucket)
   assert_that(is.character(downlogical), msg = "Downloading of file failed")
   # loading the file to the memory using user defined function
-  result = FUN(temp, ...)
-  return(result)
+  FUN(temp, envir = envir, ...)
+  return(invisible())
 }
 
--- R/import_rds.R
+++ R/import_rds.R
@@ -1,27 +1,27 @@
-#' Read RDA file
-#' @description Read RData or rda file from anywhere
+#' Read RDS file
+#' @description Read R data - RDS file from anywhere
 #' @param file path of the file to be read
 #' @param FUN the function using which the file is to be read
 #' @param data_source the name of the data source, if not set globally. s3, gcs or local
 #' @param bucket the name of the bucket, if not set globally
 #' @param ... other parameters for the FUN function defined above
-#' @export "import_rda"
+#' @export "import_rds"
 #' @return the output of the FUN function
 #'
 #' @examples
 #' \dontrun{
 #' flyio_set_datasource("gcs")
 #' flyio_set_bucket("socialcops-test")
-#' load("tests/googletest.rda")
+#' import_rds("tests/googletest.rds", readRDS)
 #' }
 
-import_rda <- function(file, FUN = load, data_source = flyio_get_datasource(),
+import_rds <- function(file, FUN = readRDS, data_source = flyio_get_datasource(),
                        bucket = flyio_get_bucket(data_source), ...){
 
   # checking if the file is valid
-  assert_that(tools::file_ext(file) %in% c("rda", "RData"), msg = "Please input a valid path")
+  assert_that(tools::file_ext(file) %in% c("RDS", "rds"), msg = "Please input a valid path")
   if(data_source == "local"){
-    FUN(file, ...)
+    t = FUN(file, ...)
     return(t)
   }
   # a tempfile with the required extension
@@ -32,7 +32,7 @@ import_rda <- function(file, FUN = load, data_source = flyio_get_datasource(),
   downlogical = import_file(bucketpath = file, localfile = temp, bucket = bucket)
   assert_that(is.character(downlogical), msg = "Downloading of file failed")
   # loading the file to the memory using user defined function
-  FUN(temp, ...)
-  return(invisible())
+  result = FUN(temp, ...)
+  return(result)
 }
 
