8430c6db747797a28f41aaf56ff52d50f17ce25e
Himanshu <himanshu@Administrators-MacBook-Pro.local>
2019-01-05 18:22:49 +0530

    fix #20

--- R/flyio_bucket.R
+++ R/flyio_bucket.R
@@ -3,7 +3,7 @@
 #' @param bucket the bucket name to be set
 #' @param data_source the data source used for I/O. Default chooses the data source set using flyio_set_datasource()
 #'
-#' @return stores the bucket name in a global environment under rioBucketGcs or rioBucketS3
+#' @return stores the bucket name in a global environment under flyioBucketGcs or flyioBucketS3
 #' @export "flyio_set_bucket"
 #' @import "stringr"
 #'
@@ -17,10 +17,10 @@ flyio_set_bucket <- function(bucket, data_source = flyio_get_datasource()){
   }
   assert_that(is.string(bucket) && bucket != "", msg = "Enter a valid bucket name")
   if(data_source == "Gcs"){
-    Sys.setenv("rioBucketGcs" = bucket)
+    Sys.setenv("flyioBucketGcs" = bucket)
     message("Default bucket name for ",data_source ," set to '",bucket,"'")
   } else if(data_source == "S3"){
-    Sys.setenv("rioBucketS3" = bucket)
+    Sys.setenv("flyioBucketS3" = bucket)
     message("Default bucket name for ",data_source ," set to '",bucket,"'")
   }
 
@@ -43,7 +43,7 @@ flyio_get_bucket <- function(data_source = flyio_get_datasource()){
   assert_that(str_to_lower(data_source) %in% c("gcs", "s3", "local"), msg = "Enter a valid data source")
   data_source = str_to_title(data_source)
   if(data_source == "Local") return("")
-  bucket = Sys.getenv(paste0("rioBucket",data_source))
+  bucket = Sys.getenv(paste0("flyioBucket",data_source))
   invisible(assert_that(is.string(bucket) && bucket != "", msg = "No bucket set. Use flyio_set_bucket to set the bucket name globally."))
   return(bucket)
 }
--- R/flyio_datasource.R
+++ R/flyio_datasource.R
@@ -2,14 +2,14 @@
 #' @description Set global data source name to be used for all the function in flyio
 #' @param data_source the DataSource name to be set
 #'
-#' @return stores the data source name in a global environment under rioDataSource
+#' @return stores the data source name in a global environment under flyioDataSource
 #' @export "flyio_set_datasource"
 #' @import "stringr"
 #' @examples flyio_set_datasource("local")
 flyio_set_datasource <- function(data_source){
   invisible(assert_that(is.string(data_source) && str_to_lower(data_source) %in% c("gcs", "s3", "local"), msg = "Enter a valid data source name"))
   data_source = str_to_lower(data_source)
-  Sys.setenv("rioDataSource" = data_source)
+  Sys.setenv("flyioDataSource" = data_source)
   message("Default Data Source name set to '",data_source,"'")
 }
 
@@ -24,7 +24,7 @@ flyio_set_datasource <- function(data_source){
 #' # getting the data source
 #' flyio_get_datasource()
 flyio_get_datasource <- function(){
-  data_source = Sys.getenv("rioDataSource")
+  data_source = Sys.getenv("flyioDataSource")
   invisible(assert_that(is.string(data_source) && data_source != "", msg = "No data source set. Use flyio_set_datasource to set the data source."))
   return(data_source)
 }
