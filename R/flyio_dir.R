e28b1ddc3557a1e9a74ce46dd9ea00c498bf6803
Himanshu Sikaria <himanshu.sikaria@socialcops.com>
2019-04-10 12:46:15 +0530

    default directory changed to flyio in tmpdir

--- R/flyio_dir.R
+++ R/flyio_dir.R
@@ -6,7 +6,10 @@
 #' @import "assertthat"
 #'
 #' @examples flyio_set_dir(dir = tempdir())
-flyio_set_dir <- function(dir = tempdir()){
+flyio_set_dir <- function(dir = paste0(tempdir(), "/flyio",Sys.getpid())){
+  if(dir == paste0(tempdir(), "/flyio",Sys.getpid()) & !dir.exists(paste0(tempdir(), "/flyio",Sys.getpid()))){
+    dir.create(paste0(tempdir(), "/flyio",Sys.getpid()))
+  }
   assert_that(is.dir(dir), msg = "Enter a valid directory name")
   Sys.setenv("CLOUD_DIR" = normalizePath(dir, mustWork = FALSE))
   message("Default directory name for flyio set to '",dir,"'")
@@ -15,7 +18,7 @@ flyio_set_dir <- function(dir = tempdir()){
 #' Get global bucket name for flyio
 #' @description Get global directory where flyio functions will download intermidiate files
 #' @return the string - directory name
-#' @details if the directory is not set using flyio_set_dir(), it will returnt he tempdir()
+#' @details if the directory is not set using flyio_set_dir(), it will return the paste0(tempdir(),"/flyio")
 #' @export "flyio_get_dir"
 #'
 #' @examples
@@ -23,7 +26,11 @@ flyio_set_dir <- function(dir = tempdir()){
 flyio_get_dir <- function(){
   dir = Sys.getenv("CLOUD_DIR")
   if(dir == ""){
-    dir = normalizePath(tempdir(), mustWork = FALSE)
+    tmpdir = normalizePath(tempdir(), mustWork = FALSE)
+    dir = paste0(tmpdir, "/flyio",Sys.getpid())
+  }
+  if(dir == paste0(tmpdir, "/flyio",Sys.getpid()) & !dir.exists(paste0(tmpdir, "/flyio",Sys.getpid()))){
+    dir.create(paste0(tmpdir, "/flyio",Sys.getpid()))
   }
   return(dir)
 }
